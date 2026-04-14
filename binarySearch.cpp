9da0d940b2456707f40447735bb4d02ccf31c040
khalid0931 <mutiarif53@gmail.com>
2026-04-14 10:38:12 +0700

    membuat void input

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -4,3 +4,62 @@ using namespace std;
 int element[10];
 int nPanjang;
 int x;
+
+void input ()
+{
+    while (true)
+    {
+        cout << "Masukan banyaknya element pada array (maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[i] jumlah element tidak boleh lebih dari sepuluh,silahkan coba lagi.\n";
+        }
+    }
+}
+
+cout << "\n===================================\n";
+cout << "          Masukan Element Array      \n";
+cout << " ====================================\n";
+
+for (int i = 0; i < nPanjang i++)
+{
+    cout << "Data ke-" << i + 1 << " = ";
+    cin >> element[i];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void display()
+{
+    cout << "\n==========================\n";
+    cout << " Element Array Setelah Diurutkan (Asc)\n"
+}
+
+
+
+
+
+
+
